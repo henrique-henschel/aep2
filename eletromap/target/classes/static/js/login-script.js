8f360b210d7f41d7d4c1e8b2f161b11d9bb15b6e
007f <88677725+007f@users.noreply.github.com>
2025-06-07 00:19:49 -0300

    Delete eletromap directory para substituir

--- eletromap/target/classes/static/js/login-script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', (event) => {
-    const credencial = document.getElementById('credencial').value.trim();
-    const password = document.getElementById('password').value.trim();
-
-    if (credencial === '' || password === '') {
-        event.preventDefault();
-        alert('Por favor, preencha todos os campos do formulário.');
-    }
-});
